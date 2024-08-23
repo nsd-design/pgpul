78d21d85123ccf1fa2238ac2f791fc569a919851
nsd-design <ibayo427@gmail.com>
2024-08-23 23:55:25 +0000

    Creation Landingpage models, config .env credentials

--- landing_page/models.py
+++ landing_page/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from tinymce.models import HTMLField
 
-# Create your models here.
+from utilisateur.models import Utilisateur
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = HTMLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(Utilisateur, on_delete=models.SET_NULL, null=True)
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=125)
+    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(Utilisateur, on_delete=models.CASCADE, null=True)
+
+
+class Partenaire(models.Model):
+    denomination = models.CharField(max_length=200)
+    logo = models.ImageField(upload_to="logo_partner/")
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(Utilisateur, on_delete=models.SET_NULL, null=True)
+
+
+class Temoignage(models.Model):
+    nom = models.CharField(max_length=120)
+    avis = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(Utilisateur, on_delete=models.SET_NULL, null=True)
+
+
+class ContactUs(models.Model):
+    email = models.CharField(max_length=200)
+    objet = models.CharField(max_length=120)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- landing_page/views.py
+++ landing_page/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 
 def home(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Vous êtes connecté avec succès !")
+            return redirect('dashmin')
+        else:
+            messages.error(request, "Identifiants incorrects. Veuillez réessayer.")
+
     return render(request, template_name="landing_page/index.html")
