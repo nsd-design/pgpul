141ea6fcfe5977ab93eb21696d8e8309ad22f7e8
nsd-design <ibayo427@gmail.com>
2024-09-08 18:16:21 +0000

    Pagination sur Temoignage,Blog et creation partner
    (cherry picked from commit 8050b9a8a2abac67e3edc00dd12d9d454981d5eb)

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -5,5 +5,7 @@ from landing_page.views import *
 urlpatterns = [
     path('blog/', blog, name='blog'),
     path('blog/read/<int:id_article>', lire_article, name='read-article'),
-    path('temoignage', temoignage, name="temoignage")
+    path('temoignage/', temoignage, name="temoignage"),
+    path('partenaire/', partenaire, name="partenaire"),
+    path('partenaire/<int:id_partner>', partenaire, name="partenaire"),
 ]
