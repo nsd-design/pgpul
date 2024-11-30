cf04538c71fe604e23e42ee0fe262760c0deb036
nsd-design <ibayo427@gmail.com>
2024-11-30 16:16:37 +0000

    Resolve 404 in Matiere, Cours et Conditionne affichage Post, part, temoignage

--- utilisateur/models.py
+++ utilisateur/models.py
@@ -3,8 +3,13 @@ from django.db import models
 
 
 class Utilisateur(AbstractUser):
+    USER_TYPE_CHOICES = (
+        ('etudiant', 'Étudiant'),
+        ('enseignant', 'Enseignant'),
+    )
     username = models.CharField(max_length=150, unique=True, blank=False)
     is_active = models.BooleanField(default=False)
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.first_name} {self.last_name} | {self.user_type}"
